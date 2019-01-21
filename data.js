c8c9b8d5f0ecf095fee124400df1278fc73d63c3
Marco - laptop <marco@marco.me>
2019-01-21 04:50:36 +0100

    first

---
+++ data.js
@@ -0,0 +1,96 @@
+data = {
+    "personal" : {
+        "name": "Marco Pascucci",
+        "email": "marcopascucci@gmail.com",
+        "telephone": "+33.(0)6.43.79.25.74",
+        "addressLine1": "64, rue Planchat",
+        "addressLine2": "75020 - Paris",
+        "motto": "Dynamic and enthusiast computer engineer with a scientific mind who always loves good challenges."
+    },
+    "educationList": 
+        [   {
+                "title": "PostDoc",
+                "where" : "LaMME",
+                "date" : "dec 2017 - now",
+                "comment" : "Laboratoire de Mathématiques et Modélisation d'Évry.  Directed a programmer intern, teaching statistics and computer languages.",
+                "display" : true
+            },
+            {
+                "title": "PhD in physics",
+                "where": "Neurophotonics lab",
+                "date" : "oct 2017",
+                "comment" : "from Paris Descartes univeristy, with a thesis in super-resolution optical microscopy. Directed an undergraduate intern.",
+                "skills" : "Image processing, signal analysis, hardware and software development using Python and Arduino",
+                "display" : true
+            },
+            {
+                "title": "MS in computer science",
+                "where" : "CNAM",
+                "date" : "dec 2016",
+                "comment" : "Secure and Embedded Systems - Conservatoire National des Arts et Metiers, with a 6 month internship developing a driver for a 2-axis plotter and a rudimental static type-checker for python.",
+                "display" : true
+            },
+            {
+                "title": "Research intern",
+                "where" : "MSC",
+                "date" : "may - june 2014",
+                "comment" : "Laboratoire Matière et Systemes Complèxes, université Paris Diderot",
+                "display" : true
+            },
+            {
+                "title": "MS bio-physics",
+                "where" : "University of Rome",
+                "date" : "may 2013",
+                "comment" : "Tor Vergata University - Rome, IT ",
+                "display" : true
+            },
+            {
+                "title" : "Summer school",
+                "where": "DESY",
+                "date" : "June – October 2011",
+                "comment" : "Deutsches Elektronen-Synchrotron - Center for Free-Electron Laser Science (CFEL). Development of a calibration software (and GUI) for x-ray detectors",
+                "display" : true
+            },
+            {
+                "title" : "Exchange student",
+                "where": "ERASMUS",
+                "date" : "Sep 2009 – Dec 2010",
+                "comment" : "at Ruprecht-Karls-Universitaet, Heidelberg (DE)",
+                "display" : true
+            },
+        ],
+    "experienceList": 
+        [
+            {
+                "title": "Lead software engieneer and project manager",
+                "where" : "MSF Foundation",
+                "date" : "dec 2017 - now",
+                "comment" : "Developing an <b>open-source</b> mobile biomedical application for Médecins Sans Frontières (MSF) Foundation. The app uses <b>AI</b> and <b>image processing</b>. Hired a software developper to comply with project deadlines.",
+                "skills": "Using: Python, Flask, openCV, C++, SQL, Android, JS, CSS, ReactJS, Git",
+                "display" : true
+            }
+        ],
+    "Miscellaneous": 
+        [
+            {
+                "where" : "Attitude",
+                "comment": "Collaborative, curious, hard working, seeking beauty in what I do. Undestanding and learning are my first motivations.",
+                "display" : true
+            },
+            {
+                "where" : "Languages",
+                "comment": "Italian (native) - French (proficient) - English (fluent) - German (good) - Spanish/portuguese (notions)",
+                "display" : true
+            },
+            {
+                "where" : "Publications",
+                "comment": "Pascucci et al. - arXiv:1710.05056 2018<br> Pascucci et al. - Physical Review Letters 2016 <br> Stellato et al. - Eur Biophys J. 2014; <br>",
+                "display" : true
+            },
+            {
+                "where" : "Interests",
+                "comment": "playing music (piano/trombone) since the age of 12, drawing, DIY, cycling.",
+                "display" : true
+            }
+        ]
+};
